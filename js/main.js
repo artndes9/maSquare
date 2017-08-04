7be57765e3e96ced921ff7ee67677a6623862b81
Basant Besra <besrabasant@gmail.com>
2017-08-04 13:21:09 +0530

    Auto stash before merge of "master" and "origin/master"

--- js/main.js
+++ js/main.js
@@ -10,7 +10,8 @@ $(document).ready(function(){
         items: 1,
         loop: true,
         nav: true,
-        dots: false
+        dots: false,
+        navText:["<span class='fa fa-chevron-left'>","<span class='fa fa-chevron-right'>"] 
     });
 
 
@@ -29,19 +30,12 @@ $(document).ready(function(){
         dots:false
     });
     function tween(slide) {
-        TweenMax.staggerFromTo(slide, 1, {opacity: 0, y: 80}, {opacity: 1, y:0}, 0.2)
+        TweenMax.staggerFromTo(slide, 1, {opacity: 0, y: 80}, {opacity: 1, y:0}, 0.1);
     }
     tween($('.owl-item.active .text').children());
 
-    var $serviceLogo = $('.serviceItem');
-    $serviceLogo.hover(
-        function(){
-            console.log($(this).find('.wrap>.icon'));
-            $(this).toggleClass("active");
-        }
-    );
+
     var $featureItem = $('.featureItem');
-    var $teamMate = $('.teammate');
     TweenMax.set($('.featureIcon'), {y:50});
     TweenMax.set($('.featureDescription'), {y:50});
     TweenMax.set($(this).find('.teammateImg .overlay'),{y:'-50%', opacity:0});
@@ -56,17 +50,6 @@ $(document).ready(function(){
             TweenMax.to($(this).find('.featureIcon'), .4, {y:50}),
             TweenMax.to($(this).find('.material-icons'), .4, {color:''}),
             TweenMax.to($(this).find('.featureDescription'), .4, {y:50, scale:0})
-        }
-    );
-    $teamMate.hover(
-        function(){
-            TweenMax.to($(this).find('.teammateImg .overlay'), 1, {y:'0%', opacity:1}),
-            TweenMax.staggerTo($(this).find('.teammateDetails').children(), .4, {y:-100, color:'#fff'}, .2),
-            TweenMax.to($(this).find('.teammateAbout'), .8, {y:-50, opacity:1, ease: CustomEase.create("custom", "M0,0,C0,0,0.454,-0.001,0.602,0.01,0.765,0.022,0.776,0.204,0.822,0.366,0.924,0.726,1,1,1,1")})
-        },function(){
-            TweenMax.to($(this).find('.teammateImg .overlay'), .4, {y:'-50%', opacity:0}),
-            TweenMax.staggerTo($(this).find('.teammateDetails').children(), .4, {y:0, color:''}, .2),
-            TweenMax.to($(this).find('.teammateAbout'), .4, {y:-100, opacity:0})
         }
     );
 
