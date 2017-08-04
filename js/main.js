f16b1e3b7d1cb80c74e5e0ec3ef513f3f1dddae3
Deepak <artndes9@gmail.com>
2017-08-04 12:58:40 +0530

    Team Section

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 $(document).ready(function(){
 
 
-    var owl = $('.owl-carousel');
+    var owl = $('#homeCarousel');
 
     owl.owlCarousel({
         items: 1,
@@ -16,11 +16,18 @@ $(document).ready(function(){
 
     owl.on('initialize.owl.carousel initialized.owl.carousel changed.owl.carousel',
         function (){
-            var slide = $('.owl-item .text').children();
+            var slide = $('#homeCarousel .owl-item .text').children();
             TweenMax.killAll();
             tween(slide)
         }
     );
+    $('#clientsCarousel').owlCarousel({
+        autoplay:true,
+        items:5,
+        nav:false,
+        margin:40,
+        dots:false
+    });
     function tween(slide) {
         TweenMax.staggerFromTo(slide, 1, {opacity: 0, y: 80}, {opacity: 1, y:0}, 0.2)
     }
@@ -36,7 +43,10 @@ $(document).ready(function(){
     var $featureItem = $('.featureItem');
     var $teamMate = $('.teammate');
     TweenMax.set($('.featureIcon'), {y:50});
-    TweenMax.set($('.featureDescription'), {y:100});
+    TweenMax.set($('.featureDescription'), {y:50});
+    TweenMax.set($(this).find('.teammateImg .overlay'),{y:'-50%', opacity:0});
+    TweenMax.set($(this).find('.teammateAbout'),{y:-100, opacity:0});
+
     $featureItem.hover(
         function(){
             TweenMax.to($(this).find('.featureIcon'), .4, {y:0}),
@@ -45,14 +55,14 @@ $(document).ready(function(){
         },function(){
             TweenMax.to($(this).find('.featureIcon'), .4, {y:50}),
             TweenMax.to($(this).find('.material-icons'), .4, {color:''}),
-            TweenMax.to($(this).find('.featureDescription'), .4, {y:100, scale:0})
+            TweenMax.to($(this).find('.featureDescription'), .4, {y:50, scale:0})
         }
     );
     $teamMate.hover(
         function(){
             TweenMax.to($(this).find('.teammateImg .overlay'), 1, {y:'0%', opacity:1}),
             TweenMax.staggerTo($(this).find('.teammateDetails').children(), .4, {y:-100, color:'#fff'}, .2),
-            TweenMax.to($(this).find('.teammateAbout'), .8, {y:-50, opacity:1})
+            TweenMax.to($(this).find('.teammateAbout'), .8, {y:-50, opacity:1, ease: CustomEase.create("custom", "M0,0,C0,0,0.454,-0.001,0.602,0.01,0.765,0.022,0.776,0.204,0.822,0.366,0.924,0.726,1,1,1,1")})
         },function(){
             TweenMax.to($(this).find('.teammateImg .overlay'), .4, {y:'-50%', opacity:0}),
             TweenMax.staggerTo($(this).find('.teammateDetails').children(), .4, {y:0, color:''}, .2),
